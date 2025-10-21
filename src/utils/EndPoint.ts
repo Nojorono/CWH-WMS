f7ca0710ff42a1c0e8c64da8240d1fcaf5aff365
DIMAS ROSADI <02000034@sonia.net>
2025-10-21 08:47:07 +0700

    comitted 21-10-2025

--- src/utils/EndPoint.ts
+++ src/utils/EndPoint.ts
@@ -1,3 +1,9 @@
-const EndPoint = "http://10.0.29.47:9005/";
-const S3EndPoint = "http://10.0.29.47:9007/s3";
+// const EndPoint = "http://10.0.29.47:9005/";
+// const S3EndPoint = "http://10.0.29.47:9007/s3";
+// export { EndPoint, S3EndPoint };
+
+
+const EndPoint = import.meta.env.VITE_API_ENDPOINT;
+const S3EndPoint = import.meta.env.VITE_S3_ENDPOINT;
+
 export { EndPoint, S3EndPoint };
